f86531d7fac1252e4666c506bc2e2e2d5c4e1e51
Amir Hamza <hamza_amir@yahoo.com>
2022-08-21 03:52:45 +0600

    create a model

---
+++ CRUD/CRUD/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
