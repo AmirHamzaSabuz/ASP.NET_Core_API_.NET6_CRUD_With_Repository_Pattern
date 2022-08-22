221500e68f643d25bf4a61ce7244b5624890aa84
Amir Hamza <hamza_amir@yahoo.com>
2022-08-22 23:00:49 +0600

    HTTP Get Rest API Method

---
+++ CRUD/CRUD/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using CRUD.Context;
+using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        ApplicationDbContext _applicationDbContext;
+        public PostController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public List<Post> GettAll() 
+        {
+            var posts = _applicationDbContext.Posts.ToList();
+            return posts;
+        }
+    }
+}
