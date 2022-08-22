4fec92f1539f23e9b7d40f81a965eeaee67ec0b3
Amir Hamza <hamza_amir@yahoo.com>
2022-08-22 21:32:54 +0600

    Add DbContext Class & Create database

---
+++ CRUD/CRUD/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
