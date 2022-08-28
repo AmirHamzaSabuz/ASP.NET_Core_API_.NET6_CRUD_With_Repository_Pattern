3864d3604afc65077e46b5c78ce94f21904ae8fe
Amir Hamza <hamza_amir@yahoo.com>
2022-08-28 08:57:33 +0600

    error handling by try catch

--- CRUD/CRUD/Controllers/PostController.cs
+++ CRUD/CRUD/Controllers/PostController.cs
@@ -20,62 +20,102 @@ namespace CRUD.Controllers
         [HttpGet]
         public IActionResult GettAll()
         {
-            var posts = _postManager.GetAll().ToList();
-            return Ok(posts);
+            try
+            {
+                var posts = _postManager.GetAll().ToList();
+                return Ok(posts);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var post = _postManager.GetById(id);
-            if (post == null)
-            { 
-                return NotFound("Data has not been found");
+            try
+            {
+                var post = _postManager.GetById(id);
+                if (post == null)
+                {
+                    return NotFound("Data has not been found");
+                }
+                return Ok(post);
             }
-            return Ok(post);
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPost]
         public IActionResult Add(Post post)
-        { 
-            post.CreatedDate = DateTime.Now;
-            bool isSaved = _postManager.Add(post);
-            if (isSaved)
+        {
+            try
             {
-                return Created("", post);
+                post.CreatedDate = DateTime.Now;
+                bool isSaved = _postManager.Add(post);
+                if (isSaved)
+                {
+                    return Created("", post);
+                }
+                return BadRequest("Post save failed");
             }
-            return BadRequest("Post save failed");
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPut]
         public IActionResult Edit(Post post)
         {
-            if (post.Id == 0)
+            try
             {
-                return BadRequest("Id is missing");
+                if (post.Id == 0)
+                {
+                    return BadRequest("Id is missing");
+                }
+                bool isUpdate = _postManager.Update(post);
+                if (isUpdate)
+                {
+                    return Ok(post);
+                }
+                return BadRequest("Post update failed");
+
             }
-            bool isUpdate = _postManager.Update(post);
-            if (isUpdate)
+            catch(Exception ex)
             {
-                return Ok(post);
+                return BadRequest(ex.Message);
             }
-            return BadRequest("Post update failed");
-        }
+        }   
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var post = _postManager.GetById(id);
-            if (post == null)
+            try
             {
-                return NotFound("Data not found");
+                var post = _postManager.GetById(id);
+                if (post == null)
+                {
+                    return NotFound("Data not found");
+                }
+                bool isDelete = _postManager.Delete(post);
+                if (isDelete)
+                {
+                    return Ok("Post has been deleted");
+                }
+                return BadRequest("Post delete faild");
             }
-            bool isDelete = _postManager.Delete(post);
-            if (isDelete)
+            catch (Exception ex)
             {
-                return Ok("Post has been deleted");
+                return BadRequest(ex.Message);
             }
-            return BadRequest("Post delete faild");
+            
         }
     }
 }
